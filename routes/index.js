33f660bfcf6c1067aa592111338908b0372b2d3d
tsim0 <tsim0@users.noreply.github.com>
2015-08-24 18:34:35 +0400

    Replace hardcoded strings with constants in Index router

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var osenv = require('osenv');
 var fs = require('fs');
+
+var RoutesPaths = require('./RoutesPaths');
+
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
 	var filesDir = osenv.home();
 	
-	var found = req.baseUrl.indexOf('/$');
+	var found = req.baseUrl.indexOf(RoutesPaths.index);
 	if (found == 0) {
-		filesDir = req.baseUrl.split('/$')[1];
+		filesDir = req.baseUrl.split(RoutesPaths.index)[1];
 	}
 
 	res.render('index', {
